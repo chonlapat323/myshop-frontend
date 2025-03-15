feedf81017657767fd972c7654e8e0390bd75a47
chonlapat323 <cschonlapat@gmail.com>
2025-03-15 16:06:53 +0700

    delete prop bestsellers component

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
     <>
       <Slider slides={slidesData} />
       <CategoryGrid limit={4} showText={true} />
-      <Bestsellers products={products} />
+      <Bestsellers />
       <ShowcaseGrid collections={collections} />
     </>
   );
