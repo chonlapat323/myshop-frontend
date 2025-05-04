e52e6f6845dce97be6d6cff56b11cc6110845322
chonlapat323 <cschonlapat@gmail.com>
2025-05-05 01:07:27 +0700

    check token

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { jwtVerify } from "jose";
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
+  console.log(token);
+  debugger;
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
