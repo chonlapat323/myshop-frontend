6a807169976688e90dc08316034796aa6597c151
chonlapat323 <cschonlapat@gmail.com>
2025-05-09 18:38:33 +0700

    feat(auth): implement member registration form with validation and API integration

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,4 +1,19 @@
+import { API_URL } from "@/lib/config";
+import { fetchWithAuth } from "@/lib/fetchWithAuth";
+import { User } from "@/types/context/auth-context";
+import { RegisterMemberDto } from "@/types/member/register-member";
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export async function registerMember(data: RegisterMemberDto): Promise<User> {
+  return fetchWithAuth<User>(`${API_URL}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
 
 export async function login(email: string, password: string) {
   const res = await fetch(`${API_BASE_URL}/auth/login_member`, {
